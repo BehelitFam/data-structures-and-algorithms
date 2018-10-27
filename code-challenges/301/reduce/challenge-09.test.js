6f1452a22c827a6ad069a93defbfbda9d70ba0df
BehelitFam <mriodine@gmail.com>
2018-10-27 16:57:34 -0700

    completed stretch goals

--- code-challenges/301/reduce/challenge-09.test.js
+++ code-challenges/301/reduce/challenge-09.test.js
@@ -228,7 +228,12 @@ const snorlaxData = {
 };
 
 const extractStat = (statName, arr) => {
-  // Solution code here...
+    return arr.reduce((acc, val) => {
+        if (val.stat.name === statName) {
+            acc = val;
+        }
+        return acc;
+    }, null);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -242,7 +247,13 @@ Write a function named extractChildren that, given the array of characters from 
 ------------------------------------------------------------------------------------------------ */
 
 const extractChildren = (arr) => {
-  // Solution code here...
+    const aListers = arr.filter(char => /a/i.test(char.name));
+    return aListers.reduce((acc, char) => {
+        if (char.children) {
+            return acc.concat(char.children);
+        }
+        return acc;
+    }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -292,13 +303,13 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return any stats that match the input', () => {
     expect(extractStat('speed', snorlaxData.stats)).toStrictEqual({ stat: { url: 'https://pokeapi.co/api/v2/stat/6/', name: 'speed' }, effort: 5, baseStat: 30 });
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return an array containing the names of the children', () => {
     expect(extractChildren(characters)).toStrictEqual([ 'Robb', 'Sansa', 'Arya', 'Bran', 'Rickon', 'Drogon', 'Rhaegal', 'Viserion', 'Margaery', 'Loras' ]);
     expect(extractChildren(characters).length).toStrictEqual(10);
